51331bde2d3ebb167d6e07647654072ce47a13bc
Oliver Rademaker <hello@oliverrademaker.de>
2026-03-13 09:13:19 +0100

    fix: sidebar deisgn with flexbox

--- src/components/sidebar.component.tsx
+++ src/components/sidebar.component.tsx
@@ -26,7 +26,7 @@ export function SidebarComponent() {
     ]
 
     return (
-        <div class="sidebar p-2" style={{ display: "grid", "grid-template-columns": "1fr", "grid-template-rows": "auto 1fr auto", "align-items": "start", gap: "2rem", height: "100%" }}>
+        <div class="sidebar p-2 d-flex flex-column gap-3 h-100">
             <ul class="nav nav-pills d-flex flex-column">
                 <For each={links}>
                     { link => (
@@ -40,37 +40,46 @@ export function SidebarComponent() {
                 </For>
             </ul>
 
-            <Suspense fallback={<div class="font-monospace" style={{ "display": "grid", "place-items": "center", "height": "100%", "width": "100%" }}>Loading...</div>}>
-                <div class="d-flex flex-column gap-2">
-                    <h1 class="fs-6 fw-normal text-muted m-0 p-0 ps-1">Repositories</h1>
-                    <ul class="nav nav-pills d-flex flex-column">
-                        <For each={repositories()}>
-                            {repo => (
-                                <li class="nav-item">
-                                    <A href={repo.path} class="nav-link d-flex" style={{ gap: "1rem" }}>
-                                        <i class={`bi bi-${repo.icon}`} />
-                                        <span>{repo.label}</span>
-                                    </A>
-                                </li>
-                            )}
-                        </For>
-                    </ul>
-                </div>
-            </Suspense>
+            <div style={{"flex-grow": 1}}>
+                <Suspense fallback={<div class="font-monospace" style={{ "display": "grid", "place-items": "center", "height": "100%", "width": "100%" }}>Loading...</div>}>
+                    <div class="d-flex flex-column gap-2">
+                        <h1 class="fs-6 fw-normal text-muted m-0 p-0 ps-1">Repositories</h1>
+                        <ul class="nav nav-pills d-flex flex-column">
+                            <For each={repositories()}>
+                                {repo => (
+                                    <li class="nav-item">
+                                        <A href={repo.path} class="nav-link d-flex" style={{ gap: "1rem" }}>
+                                            <i class={`bi bi-${repo.icon}`} />
+                                            <span>{repo.label}</span>
+                                        </A>
+                                    </li>
+                                )}
+                            </For>
+                        </ul>
+                    </div>
+                </Suspense>
+            </div>
 
-            <Switch fallback={<div class="alert alert-info text-center font-monospace">Retrieving restic version...</div>}>
+            <Switch>                          
                 <Match when={version.state === "ready"}>
                     <div class="alert alert-info font-monospace m-0">
                         <div style={{ display: "grid", "grid-template-columns": "auto 1fr", "row-gap": ".25rem", "column-gap": "1rem", "font-size": "0.75rem"}}>
                             <span>Restic:</span> <span>{version()?.version}</span>
                             <span>Go:</span><span>{version()?.go_version.replace(/go/g, "")}</span>
                         </div>
-                        
                     </div>
                 </Match>
 
-                <Match when={version.error}>
-                    <div class="alert alert-danger text-center font-monospace">{version.error}</div>
+                <Match when={version.state === "unresolved" || version.state === "pending" || version.state === "refreshing"}>
+                    <div class="alert alert-info text-center font-monospace m-0a">
+                        Retrieving restic version...
+                    </div>
+                </Match>
+
+                <Match when={version.state === "errored"}>
+                    <div class="alert alert-danger text-center font-monospace m-0">
+                        {version.error}
+                    </div>
                 </Match>
             </Switch>
         </div>
